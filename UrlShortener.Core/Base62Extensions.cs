9e5ebd466808d96a8a50eb41caa0c980825335e8
BUARI AYOTOLA <buarisulaimon@gmail,com>
2025-07-03 02:03:32 +0200

    add base 62 encoding

---
+++ UrlShortener.Core/Base62Extensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+
+namespace UrlShortener.Api.Core.Tests
+{
+    public static class Base62Extensions
+    {
+        private const string Base62Chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        public static string EncodeToBase62(this int number)
+        {
+            var result = new StringBuilder();
+            while (number > 0)
+            {
+                result.Insert(0, Base62Chars[number % 62]);
+                number /= 62;
+            }
+            return result.ToString();
+        }
+    }
+}
